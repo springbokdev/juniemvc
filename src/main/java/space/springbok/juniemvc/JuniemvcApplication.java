26d07e800b094d62534e94fe17abe57959d40b45
johnspangenberg <dev@springbok.space>
2026-03-27 17:05:41 +0100

    initial commit

---
+++ src/main/java/space/springbok/juniemvc/JuniemvcApplication.java
@@ -0,0 +1,13 @@
+package space.springbok.juniemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniemvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuniemvcApplication.class, args);
+    }
+
+}
