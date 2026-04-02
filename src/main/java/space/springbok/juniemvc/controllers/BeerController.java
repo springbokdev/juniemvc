50d77f49c7bf9953d71e035052af2048ce5fb44a
johnspangenberg <dev@springbok.space>
2026-04-02 16:00:13 +0200

    add Javadoc comments to BeerController methods for improved documentation and clarity

--- src/main/java/space/springbok/juniemvc/controllers/BeerController.java
+++ src/main/java/space/springbok/juniemvc/controllers/BeerController.java
@@ -8,6 +8,9 @@ import space.springbok.juniemvc.services.BeerService;
 
 import java.util.List;
 
+/**
+ * Controller for managing Beer operations.
+ */
 @RestController
 @RequestMapping("/api/v1/beer")
 @RequiredArgsConstructor
@@ -15,27 +18,59 @@ public class BeerController {
 
     private final BeerService beerService;
 
+    /**
+     * List all beers.
+     *
+     * @return a list of all beers.
+     */
     @GetMapping
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
+    /**
+     * Get a beer by its ID.
+     *
+     * @param beerId the ID of the beer to retrieve.
+     * @return the beer if found.
+     * @throws NotFoundException if the beer is not found.
+     */
     @GetMapping("/{beerId}")
     public Beer getBeerById(@PathVariable("beerId") Integer beerId) {
         return beerService.getBeerById(beerId).orElseThrow(NotFoundException::new);
     }
 
+    /**
+     * Save a new beer.
+     *
+     * @param beer the beer object to save.
+     * @return the saved beer.
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Beer saveNewBeer(@RequestBody Beer beer) {
         return beerService.saveNewBeer(beer);
     }
 
+    /**
+     * Update an existing beer by its ID.
+     *
+     * @param beerId the ID of the beer to update.
+     * @param beer   the updated beer object.
+     * @return the updated beer.
+     * @throws NotFoundException if the beer to update is not found.
+     */
     @PutMapping("/{beerId}")
     public Beer updateBeerById(@PathVariable("beerId") Integer beerId, @RequestBody Beer beer) {
         return beerService.updateBeerById(beerId, beer).orElseThrow(NotFoundException::new);
     }
 
+    /**
+     * Delete a beer by its ID.
+     *
+     * @param beerId the ID of the beer to delete.
+     * @throws NotFoundException if the beer to delete is not found.
+     */
     @DeleteMapping("/{beerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBeerById(@PathVariable("beerId") Integer beerId) {
